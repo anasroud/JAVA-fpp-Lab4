cdec302136c8b79ac150b6953205af8e42e52050
AliElgazzar <88932189+AliElgazzar@users.noreply.github.com>
2024-12-05 17:36:49 -0600

    Update Product.java

--- ProductPricingProject/src/Product.java
+++ ProductPricingProject/src/Product.java
@@ -1,6 +1,11 @@
 public class Product {
     public String productName;
     private double price;
+    public Product(String productName , double price)
+    {
+        this.productName=productName;
+        this.price=price;
+    }
 
     public double getPrice() {
         return price;
